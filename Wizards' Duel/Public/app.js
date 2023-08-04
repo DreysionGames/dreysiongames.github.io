6c67904a1855b9473bf1cca6f1203884e27d56b7
Dreysion <dreysion@gmail.com>
2023-08-03 22:33:30 -0500

    Can connect/disconnect players properly

--- Wizards' Duel/Public/app.js
+++ Wizards' Duel/Public/app.js
@@ -1,13 +1,80 @@
-let socket = io();
+let socket = io.connect('http://localhost:3000');
 
-console.log("works");
-
-start = document.getElementById("Start");
-start.addEventListener('click', () => {
-    console.log("pressed");
+document.getElementById("Start").addEventListener('click', () => {
     socket.emit('startGame');
 });
+document.getElementById("rename").addEventListener('click', () => {
+    pName = document.getElementById("newname").value;
+    socket.emit('rename', {
+        id: id,
+        newName: document.getElementById("newname").value
+    });
+});
+var templatePickCards
+var templateDrawCards
+var templateDrawTokens
 
+var id;
+var pName;
+
+socket.on('id', (data) => {
+    id=data.id;
+});
+socket.on('names', (data) => {
+    document.getElementById("names").innerHTML=data.nameList.map(p => p).join('<br>');
+})
+socket.on('ready', (data) => {
+    ready=[];
+    for(i=0;i<data.readyList.length;i++){
+        ready[i]="❌";
+        if(data.readyList[i]==1) ready[i]="✅";
+    }
+    document.getElementById("ready").innerHTML=ready.map(r=>r).join('<br>');
+});
 socket.on('startGame', () => {
     console.log('game Started');
+});
+
+socket.on('pickCards', (data) => {
+    console.log("drawing ${data.draw} cards from the ${data.type} pile, pick ${data.pick}");
+});
+
+socket.on('drawCards', (data) => {
+    console.log("drawing ${data.draw} cards from the ${data.type} pile, received ${data.cardsList}");
+});
+
+socket.on('drawTokens', (data) => {
+    console.log("drawing ${data.number} tokens, received ${data.tokensList}");
+});
+
+socket.on('drawEvent', (data) => {
+    if(data.event=="monster"){
+        console.log("A monster is attacking you!");
+        return;
+    }
+    if(data.event=="artifact"){
+        console.log("You discovered a new artifact!");
+        return;
+    }
+    if(data.event=="skill"){
+        console.log("You're learning a new skill!");
+        return;
+    }
+    console.log("A special event is occurring!");
+});
+
+socket.on('switchTurn', (data) => {
+    console.log("It's ${data.player}'s turn now!");
+});
+
+socket.on('skillOption', () => {
+    console.log("It's your turn to use a skill, do you want to?");
+});
+
+socket.on('reactOption', (data) => {
+    console.log("${data.attacker} is using a skill against you, do you want to react?");
+});
+
+socket.on('continueOption', (data) => {
+    console.log("Your ${data.skill} is activated and consuming energy. Do you want to keep it going?");
 });
--- Wizards' Duel/server.js
+++ Wizards' Duel/server.js
@@ -11,28 +11,278 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+var gameState = "Starting";
+var next;
+
+var players = [];
+var currentPlayer;
+
 server.listen(port, () => {
-    console.log("Listening on port ${port}");
+    console.log(`Listening on port ${port}`);
 });
 
 io.on('connection', (socket) => {
-	console.log('A user just connected.');
+    players[players.length] = new Player(socket.id);
+	console.log(`${players[players.length-1].name} just connected.`);
+    Names();
+    Ready();
     socket.on('disconnect', () => {
-        console.log('A user has disconnected.');
+        for(i=0;i<players.length;i++){
+            if(players[i].id == socket.id){
+                console.log(`${players[i].name} has disconnected.`);
+                players[i] = null;
+                players=players.filter((player) => player !== null && player !== undefined);
+            }
+        }
+        Names();
+        Ready();
+    });
+    socket.on('rename', (data) => {
+        console.log(`${findPlayer(socket.id).name} is now named ${data.newName}`);
+        findPlayer(socket.id).name = data.newName;
+        Names();
     });
     socket.on('startGame', () => {
-        io.emit('startGame');
+        console.log(`${findPlayer(socket.id).name} is ready to start the game`)
+        findPlayer(socket.id).ready=1;
+        if(Ready()){
+            next=0;
+            io.emit('startGame');
+        }
+    });
+    socket.on('pickCards', (data) => {
+        //Check to see the picked cards match the list sent to them in the first place
+        console.log("${data.player} picked: ${data.list}");
+        //update player's card list
+    });
+    socket.on('endTurn', () => {
+        //player++
+        next=0;
+        io.emit('switchTurn', {
+            player: player
+        });
+    });
+    socket.on('accept', () => {
+        if(Ready()){Next(true);}
+    });
+    socket.on('decline', () => {
+        Next(false);
     });
 });
 
+function Names(){
+    var names = players.map((p) => p.name);
+    io.emit('names', {
+        nameList: names
+    });
+}
 
-/*app.get('/', (req, res) => res.send('Welcome To Nodejs!'));
-app.listen(3000, () => console.log(`Example app listening on port ${port}!`));*/
+function Ready(){
+    var ready = players.map((r) => r.ready);
+    io.emit('ready', {
+        readyList: ready
+    });
+    for(i=0;i<ready.length;i++){
+        if(ready[i]==0 || ready[i]==undefined){return false;}
+    }
+    return true;
+}
 
-/*var server = require("http").createServer(app).listen(process.env.PORT || 8080);
-var io = require("socket.io").listen(server);
+function Next(val){
+    for(i=0;i<players.length;i++){
+        players[i].ready=0;
+        Ready();
+    }
+    if(gameState=="Starting"){
+        switch(next){
+            case 0:
+                //except need different cards for each player, cache the results for later verification
+                io.emit('pickCards', {
+                    type: "skill",
+                    draw: 5,
+                    pick: 2,
+                    list: [
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)]
+                    ]
+                });
+                break;
+            case 1:
+                io.emit('pickCards', {
+                    type: "artifact",
+                    draw: 3,
+                    pick: 1,
+                    list: [
+                        artifactPool[Math.floor(Math.random()*artifactPool.length)],
+                        artifactPool[Math.floor(Math.random()*artifactPool.length)],
+                        artifactPool[Math.floor(Math.random()*artifactPool.length)]
+                    ]
+                });
+                break;
+            case 2:
+                io.emit('drawTokens', {
+                    draw: 5,
+                    list: [
+                        tokenPool[Math.floor(Math.random()*4)],
+                        tokenPool[Math.floor(Math.random()*4)],
+                        tokenPool[Math.floor(Math.random()*4)],
+                        tokenPool[Math.floor(Math.random()*4)],
+                        tokenPool[Math.floor(Math.random()*4)]
+                    ]
+                });
+                break;
+            case 3:
+                io.emit('role'); //player turn order
+                break;
+            case 4:
+                //set action/reaction cards
+                io.emit('setCards');
+                break;
+            case 5:
+                gameState="Training";
+                next=0;
+        }
+        return;
+    }
+    if(gameState=="Training"){
+        switch(next){
+            case 0:
+                io.emit('drawTokens', {
+                    draw: 2,
+                    list: [
+                        tokenPool[Math.floor(Math.random()*4)],
+                        tokenPool[Math.floor(Math.random()*4)]
+                    ]
+                })
+                break;
+            case 1:
+                if(player.enemies){
+                    //enemies attack
+                    break;
+                }
+                next++;
+            case 2:
+                io.emit('role'); //check for encounters
+                break;
+            case 3:
+                io.emit('skillOption');
+                break;
+            case 4:
+                //only emit to single player if attack
+                io.emit('reactOption', {
+                    attacker: currentPlayer
+                });
+        }
+        return;
+    }
+    if(gameState=="Competing"){
+        switch(next){
+            case 0:
+                //announce bracket
+                break;
+            case 1:
+                io.emit('pickCards',{
+                    type: "skill",
+                    draw: 3,
+                    pick: 1,
+                    list: [
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)],
+                        skillPool[Math.floor(Math.random()*skillPool.length)]
+                    ]
+                });
+                break;
+            case 2:
+                io.emit('skillOption');
+                break;
+            case 3:
+                //if applicable
+                io.emit('reactOption', {
+                    attacker: currentPlayer
+                });
+        }
+        return;
+    }
+    next++;
+}
 
-io.sockets.on("connection", function(socket) {
+const skillPool = [
+    ["Fireball", 5],
+    ["Icicle", 5]
+];
 
-    console.log("test");
-});*/
+var skillWeight=0;
+for(i=0;i<skillPool.length;i++){
+    skillWeight+=skillPool[i][1];
+}
+
+const artifactPool = [
+    
+];
+
+var artifactWeight=0;
+for(i=0;i<artifactPool.length;i++){
+    artifactWeight+=artifactPool[i][1];
+}
+
+const eventPool = [
+
+];
+
+var eventWeight=0;
+for(i=0;i<eventPool.length;i++){
+    eventWeight+=eventPool[i][1];
+}
+
+const tokenPool = [
+    "fire",
+    "water",
+    "earth",
+    "air"
+];
+
+const enemyPool = [
+
+];
+
+class Player {
+    constructor(id) {
+        this.id = id;
+        this.name = "Player " + Math.floor(Math.random()*10000);
+        this.health = 25;
+        this.ready = 0;
+
+        this.energy = new Energy(0, 0, 0, 0);
+        this.skills = [];
+        this.active = [];
+        this.actions = [];
+        this.reacions = [];
+        this.artifacts = [];
+        this.targets = [];
+    }
+}
+
+class Energy {
+    constructor(fire, water, earth, air) {
+        this.fire = fire;
+        this.water = water;
+        this.earth = earth;
+        this.air = air;
+
+        this.subtract = function (Energy2) {
+            return new Energy(this.fire - Energy2.fire, this.water - Energy2.water, this.earth - Energy2.earth, this.air - Energy2.air);
+        };
+        this.add = function (Energy2) {
+            return new Energy(this.fire + Energy2.fire, this.water + Energy2.water, this.earth + Energy2.earth, this.air + Energy2.air);
+        };
+    }
+}
+
+function findPlayer(id){
+    for(i=0;i<players.length;i++){
+        if(players[i].id==id) return players[i];
+    }
+}
